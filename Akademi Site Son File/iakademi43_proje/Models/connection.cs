668b80fa5bf1ebc434a5dde39b03af38cfdccb72
elmasahmet <95107628+elmasahmet@users.noreply.github.com>
2022-08-29 14:05:33 +0300

    E-Ticaret Sitesi
    C# yazılım dili ile oluşturmuş olduğum E-Ticaret Sitesi

---
+++ Akademi Site Son File/iakademi43_proje/Models/connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace iakademi43_proje.Models
+{
+    public class connection
+    {
+
+        public static SqlConnection baglanti
+        {
+            get
+            {
+                SqlConnection sqlcon = new SqlConnection("Server=.;Database=iakademi43DB_Proje;trusted_connection=True;");
+                return sqlcon;
+            }
+        }
+
+    }
+}
